0e000165c64bd55bb245403c8419852d21236b30
Aditya Wicaksono <dityawicaksono12@gmail.com>
2025-06-22 20:35:40 -0400

    Js get element by id

---
+++ script.js
@@ -0,0 +1,7 @@
+let task = []
+
+const nameInput = document.getElementById("taskName")
+const categoryInput = document.getElementById("taskCategory")
+const deadlineInput = document.getElementById("taskDeadline")
+const statusInput = document.getElementById("taskStatus")
+const addBtn = document.getElementById("addTaskBtn")
