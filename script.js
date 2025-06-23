528408fa3be414ba29600e05868b6c68999788fa
Aditya Wicaksono <dityawicaksono12@gmail.com>
2025-06-22 22:32:17 -0400

    add showtask function

--- script.js
+++ script.js
@@ -28,33 +28,33 @@ function clearInputs(){
     statusInput.value = "In Progress"
 }
 
-function showTasks(){
-    taskList.innerHTML = ""
-    checkOverdue()
-
-    const sFilter = statusFilter.value
-    const cFilter = categoryFilter.value.toLowerCase()
-
-    for (let i = 0; i < tasks.length, i++){
-        const task = tasks[i]
-        const matchStatus = !sFilter || task[3] === sFilter
-        const matchCategory = !cFilter || task[1].toLowerCase().includes(cFilter)
-
-        if (matchStatus && matchCategory){
-            const li = document.createElement("li")
-            li.innerHTML = '
-                ${task[0]} | ${task[1]} | ${task[2]} |
-                <select data-index="${1}">
-                    <option ${task[3] === "In Progress" ? "selected" : ""}>In Progress</option>
-                    <option ${task[3] === "Completed" ? "selected" : ""}>Completed</option>
-                    <option ${task[3] === "Overdue" ? "selected" : ""}>Overdue</option>
-                </select>
-                <button data-delete="${i}">Delete</button>
-            '
-            taskList.appendChild(li)
-        }
+function showTasks() {
+    taskList.innerHTML = "";
+    checkOverdue();
+  
+    const sFilter = statusFilter.value;
+    const cFilter = categoryFilter.value.toLowerCase();
+  
+    for (let i = 0; i < tasks.length; i++) {
+      const task = tasks[i];
+      const matchStatus = !sFilter || task[3] === sFilter;
+      const matchCategory = !cFilter || task[1].toLowerCase().includes(cFilter);
+  
+      if (matchStatus && matchCategory) {
+        const li = document.createElement("li");
+        li.innerHTML = `
+          ${task[0]} | ${task[1]} | ${task[2]} | 
+          <select data-index="${i}">
+            <option ${task[3] === "In Progress" ? "selected" : ""}>In Progress</option>
+            <option ${task[3] === "Completed" ? "selected" : ""}>Completed</option>
+            <option ${task[3] === "Overdue" ? "selected" : ""}>Overdue</option>
+          </select>
+          <button data-delete="${i}">Delete</button>
+        `;
+        taskList.appendChild(li);
+      }
     }
-
-    setupStatusDropdown()
-    setupDeleteButtons()
-}
+  
+    setupStatusDropdowns();
+    setupDeleteButtons();
+  }
