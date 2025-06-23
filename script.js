45decfcf55adefe6d5c62e11200a72807dd25cec
Aditya Wicaksono <dityawicaksono12@gmail.com>
2025-06-22 20:40:16 -0400

    add event listener

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
-let task = []
+let tasks = []
 
 const nameInput = document.getElementById("taskName")
 const categoryInput = document.getElementById("taskCategory")
 const deadlineInput = document.getElementById("taskDeadline")
 const statusInput = document.getElementById("taskStatus")
 const addBtn = document.getElementById("addTaskBtn")
+
+addBtn.addEventListener("click", function(){
+    const task = [
+        nameInput.value,
+        categoryInput.value,
+        deadlineInput.value,
+        statusInput.value
+    ]
+    if (task[0] && task[1] && task[2]){
+        tasks.push(task)
+        saveTasks()
+        clearInputs()
+        showTasks()
+    }
+})
+
